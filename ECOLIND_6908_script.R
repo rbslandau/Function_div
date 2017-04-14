d3269340d939b8eb339c10b53b82b8120934dcbc
rbslandau <schaefer-ralf@uni-landau.de>
2017-04-14 11:17:25 +0200

    final revision changes

--- ECOLIND_6908_script.R
+++ ECOLIND_6908_script.R
@@ -17,21 +17,24 @@
 #
 ########################################################################################
 
-# Code written by K Voß, revised by RB Schäfer
+# Code written by K Voß, in major parts rewritten by RB Schäfer
 ## The code has been written for application to the supplied data sets
 
 # Structure of the code:
 # 1 # Gradient building ---------------------------------------------
 # 2 # Diversity metrics and gradient --------------------------------
-# 3 # Diversity metrics and Organic matter breakdown-----------------
-# 4 # Stressor effect on invertertebrate community structure (RDA)---
-# 5 # Stressor effect on community trait structure (RDA) ------------
+# 3 # Stressor effect on invertertebrate community structure (RDA)---
+# 4 # Stressor effect on community trait structure (RDA) ------------
+# 5 # Diversity metrics and Organic matter breakdown-----------------
 # Fig.1 and Fig.2 from the article ----------------------------------
 # Fig. S1 and S2 from Supplementary Informations---------------------
-
-###### add github links?
-
 # See README.txt for details
+
+# Set path to working directory, where files and history is stored
+prj <- "~/Desktop/ECOLIND-6908_Voss_Schaefer/data"
+# you need to replace this line with a path on your computer
+setwd(prj)
+
 rm(list = ls())
 
 # Libraries Used
@@ -45,11 +48,31 @@ lapply(pkg, library, character.only=T)
 ### Setup project
 ##################
 
-# Set path to working directory, where files and history would be stored
-prj <- "~/Desktop/ECOLIND-6908_Voss_Schaefer/data"
-setwd(prj)
-# load data
+# load variables data
 parameter <- read.csv("https://raw.githubusercontent.com/rbslandau/Function_div/master/variables_final.csv", sep=";", dec=".", header=T, check.names=F) 
+
+# load trait data for diversity indices (trait - species matrix)
+trait <- read.csv("https://raw.githubusercontent.com/rbslandau/Function_div/master/ID_traits.csv", sep= "", header=T, as.is =TRUE) 
+
+# read trait labels for later labelling as function for calculation of FD requires neutral labels
+trait_labels <- read.csv("https://raw.githubusercontent.com/rbslandau/Function_div/master/Labels_traits.csv", sep="\t", header=T, check.names=F)
+
+# load taxonomic data for diversity indices (species - site matrix)
+abun <- read.csv("https://raw.githubusercontent.com/rbslandau/Function_div/master/ID_abundance.csv", sep= "", header=T) 
+
+# load data on organic matter breakdown
+omb_dat <- read.csv("https://raw.githubusercontent.com/rbslandau/Function_div/master/omb.csv", sep= "", header=T, as.is =TRUE)
+
+# load taxonomic data for RDA (species - site matrix, but with real species names)
+taxa <- read.csv("https://raw.githubusercontent.com/rbslandau/Function_div/master/inv_taxa_rda.csv", sep="\t", header=T, check.names=F)
+# assign site code to rownames
+rownames(taxa) <- taxa[ ,1]
+# remove ID row and site information
+taxa_new <- taxa[-1, -1]
+
+str(taxa_new)
+# convert to numeric variable
+taxa_new[1:67] <- lapply(taxa_new[1:67], as.numeric)
 
 ####################
 # 1. Build gradient
@@ -119,33 +142,43 @@ grad <- unlist(pca_axes[ ,1]) * -1
 # 2.    Diversity metrics and gradient
 ####################################################
 
-# load data on taxonomic diversity metrics and relative abundance
-# of each trait modality within the communities
-data <- read.csv("https://raw.githubusercontent.com/rbslandau/Function_div/master/diversities.csv", sep="\t", dec=".", header=T, check.names=F)
-str(data)
-
+# calculate TTR and SD
+TTR <- specnumber(abun, MARGIN = 1)
+SD <- diversity(abun, index = "invsimpson", MARGIN = 1)
 # relationship between taxonomic diversities (Total taxonomic richness and Simpsons Diversity) and gradient
 
 ########################
 # Taxonomic diversity
 ########################
-# linear regression for taxonomic diversity
-m1 <- cor.test(data$TTR, grad)
-m2 <- cor.test(data$SD, grad)
+# correlation for taxonomic diversity
+m1 <- cor.test(TTR, grad)
+m2 <- cor.test(SD, grad)
 
 p.adjust(c(m1$p.value, m2$p.value), method= "fdr")
+# strong response of TTR compared to SD suggests that mainly rare species were lost
 
-# for plot refer to section 5
+# recalculate SD downweighing dominant taxa
+SD_dw <- diversity(sqrt(abun), index = "invsimpson", MARGIN = 1)
+
+(m2_1 <- cor.test(SD_dw, grad))
+
+# identify dominant species
+# use taxa data because contains species labels
+tot_abun_spec <- colSums(taxa_new)
+sort(tot_abun_spec)
+# gammarids dominate abundance
+
+# compute fraction of gammarids per site
+tot_gam <- rowSums(taxa_new[ ,names(taxa_new) %in% c("Gammarus roeseli", "Gammarus fossarum", "Gammarus pulex")])
+tot_abun <- rowSums(taxa_new)
+frac_gam <- tot_gam/tot_abun
+# gammarids abundance > 50% in 75% of sites, median: 79%
 
 ###########################################################################################################
-# Calculation of FD: functional richness, evenness, divergence over all trait modalities for each community
-# and for OMB-relevant traits seperately for each community
-# to relate it to gradient with simple regression
+# Calculation of FD: functional richness, evenness, divergence over all trait modalities
+# and over OMB-relevant traits for each community
+# to check for correlation
 ###########################################################################################################
-
-# load data
-trait <- read.csv("https://raw.githubusercontent.com/rbslandau/Function_div/master/ID_traits.csv", sep= "", header=T, as.is =TRUE) 
-abun <- read.csv("https://raw.githubusercontent.com/rbslandau/Function_div/master/ID_abundance.csv", sep= "", header=T) 
 
 # calculating proportions of single trait modalities per trait for the fuzzy coded data
 col.blocks <- c(7,2,3,4,8,4, 5,5,8,9,8,7,8,3,9,4,3,2,3,5,6)
@@ -153,42 +186,31 @@ prop_trait <- prep.fuzzy.var(trait, col.blocks, row.w = rep(1, nrow(trait)))
 # write.csv(prep.fuzzy.var(trait, col.blocks, row.w = rep(1, nrow(trait))))#, file = "prop_traits.csv")
 
 # calculating distance-based functional diversity indices
-fds <- dbFD(x = prop_trait, a = abun, stand.FRic = TRUE)
+fds <- dbFD(x = prop_trait, a = abun, stand.FRic = TRUE, w.abun = TRUE)
 str(fds)
-data$overall_FRic <- fds$FRic
-data$overall_FEve <- fds$FEve
-data$overall_FDiv <- fds$FDiv
+FD <- NULL
+FD$overall_FRic <- fds$FRic
+FD$overall_FEve <- fds$FEve
+FD$overall_FDiv <- fds$FDiv
 # uncomment for calculating functional redundancy according to Mouillotet al. 2014. Proceedings of the National Academy of Sciences 111, 13757–13762. doi:10.1073/pnas.1317625111
-# data$overall_FRed <- fds$sing.sp/fds$nbsp
- 
-# some modalities have been converted to factors
-# because all values are 0
-# replace with 0
-modalities <- fds$CWM
-temp_facmod <- sapply(modalities,is.factor)
-modalities[ ,temp_facmod] <- 0  
- 
+# FD$overall_FRed <- fds$sing.sp/fds$nbsp
+  
 ##############################################################################
-## Functional diversity of OMB-related traits (food and feeding habit)
+## Functional diversity only for OMB-related traits (food and feeding habit)
 
-fds_Food <- dbFD(x = prop_trait[ , c(47:63)], a = abun, stand.FRic = TRUE)
+fds_Food <- dbFD(x = prop_trait[ , c(47:63)], a = abun, stand.FRic = TRUE, w.abun = TRUE)
 str(fds_Food)
 
-data$OMB_FRic <- fds_Food$FRic
-data$OMB_FEve <- fds_Food$FEve
-data$OMB_FDiv <- fds_Food$FDiv
+FD$OMB_FRic <- fds_Food$FRic
+FD$OMB_FEve <- fds_Food$FEve
+FD$OMB_FDiv <- fds_Food$FDiv
 # uncomment for calculating functional redundancy
-# data$OMB_FRed <- fds_Food$sing.sp/fds_Food$nbsp
+# FD$OMB_FRed <- fds_Food$sing.sp/fds_Food$nbsp
 
 ########################
 #Functional Diversities
 ########################
-
-#extract FD indices
-FD <- data[ ,120:125]
-# FD <- data[ ,120:127]
-# uncomment if including functional redundancy
-
+# correlations
 str(FD)
 res <- sapply(FD, function(y) {
   test <- cor.test(y, grad, method="pearson")
@@ -205,32 +227,35 @@ names(fin_FD2)[1:2] <- c("Correl_coeff", "p_adjusted")
 # save (cf. Tab.S5)
 # write.csv(fin_FD2, file="cor_FD_gradient.csv", row.names=F)
 
+fin_FD2
+# no significant correlation
+# although moderate correlation of functional richness with stress gradient
+
 ######################################################################
-# Relative abundance of trait modalities (n=113) and gradient
+# Check for correlation with individual modalities
 ######################################################################
+# some modalities have been converted to factors
+# because all values were 0
+# replace with numeric 0
+modalities <- fds$CWM
+temp_facmod <- sapply(modalities,is.factor)
+modalities[ ,temp_facmod] <- 0
 
-# extract trait modalities
-str(modalities)
+# extract the two modalities
+ma <- cor.test(modalities$t10.3, grad)
+mb <- cor.test(modalities$t11.3, grad)
 
-res1 <- sapply(modalities, function(y) {
-  test1 <- cor.test(y, grad, method="pearson")
-})
+p.adjust(c(ma$p.value, mb$p.value), method= "fdr")
+# dead plant as food -0.18
+# being shredder -0.21
 
-# extract coef. of correlation
-cor1 <- res1[4,]
-value1 <- res1[3,]
-p.adjusted_2 <- p.adjust(value1,method= "fdr")
-fin_modal <- rbind(unlist(cor1), unlist(p.adjusted_2))
-fin_modal2 <- as.data.frame(t(fin_modal))
-fin_modal2$"Modality" <- row.names(fin_modal2)
-names(fin_modal2)[1:2] <- c("Correl_coeff", "p")
-sum(fin_modal2$p < 0.05, na.rm = TRUE) # sum of significant p values
-# save (cf. Tab. S6)
-# write.csv(fin_modal2, file="cor_modalities_gradient.csv", row.names = F)
+plot(grad, modalities$t11.3)
+# no relationship
 
+# May be both modalities are relatively constant?
 # summary of variation in both OMB-related traits
-summary(fds$CWM$t10.3)
-summary(fds$CWM$t11.3)
+summary(modalities$t10.3)
+summary(modalities$t11.3)
 # mean and standard deviation
 mean(fds$CWM$t10.3)
 sd(fds$CWM$t10.3) 
@@ -238,85 +263,26 @@ sd(fds$CWM$t10.3)
 mean(fds$CWM$t11.3)
 sd(fds$CWM$t11.3) 
 
-# computation of total trait abundance per site
-trait_abun_matx <- as.matrix(abun) %*% as.matrix(prop_trait)
-
-############################################################
-## 3.  Diversity metrics and Organic matter breakdown (OMB)
-############################################################
-
-# Analyse whether changes in taxonomic and functional diversity propagate to OMB
-# relationship between taxonomic diversities and Organic matter breakdown
-# relationship between functional diversities (two OMB-relevant FDs and their modalities) and OMB
-# simple regression with pearson correlation
-
-# Taxonomic diversity with OMB
-m3 <- cor.test(data$TTR, data$OMB)
-m4 <- cor.test(data$SD, data$OMB)
-p.adjust(c(m3$p.value, m4$p.value), method= "fdr")
-
-#for plot refer to section 5
-
-############################################
-#Functional Diversities and OMB
-############################################
-
-res_fd <- sapply(FD, function(y) {
-  test <- cor.test(y, data$OMB, method="pearson")
-})
-
-# extract coef. of correlation
-cor_fd <- res_fd[4, ]
-value_fd <- res_fd[3, ]
-p.adjusted2 <- p.adjust(value_fd, method= "fdr")
-
-# OMB relevant food and feeding trait and OMB
-OMB_mod <- modalities[ ,c(47:63)]
-
-res2 <- sapply(OMB_mod, function(y) {
-  test <- cor.test(y, data$OMB, method="pearson")
-})
-
-# extract coef. of correlation
-cor2 <- res2[4, ]
-value2 <- res2[3, ]
-p.ad2 <- p.adjust(value2, method= "fdr")
-fin_OMB <- rbind(unlist(cor2), unlist(p.ad2))
-fin_OMB2 <- as.data.frame(t(fin_OMB))
-fin_OMB2$"OMBrelevant_mod" <- row.names(fin_OMB2)
-names(fin_OMB2)[1:2] <- c("Correl_coeff", "p")
-
-# save (cf. Tab.S7)
-# write.csv(fin_OMB2, file="cor_OMBmod_OMB.csv", row.names=F)
-
-# check summed abundance for specific modalities 
-<- cor.test(trait_abun_matx[ ,"t10.3"], data$OMB)
-<- cor.test(trait_abun_matx[ ,"t11.3"], data$OMB)
-
 #############################################################
-# 4.  Redundancy Discriminant Analysis for invertebrate taxa#
+# 3.  Redundancy Discriminant Analysis for invertebrate taxa#
 #############################################################
 
-taxa <- read.csv("https://raw.githubusercontent.com/rbslandau/Function_div/master/inv_taxa_rda.csv", sep="\t", header=T, check.names=F)
-rownames(taxa) <- taxa[ ,1]
-aggr_taxa <- taxa[-1, ]
-#omit ID's
-
-# aggregate some species with same trait information and where species are rare 
+# aggregate some species with same trait information and where species are rare
 # (Cordulegaster sp., Philopotamus sp., Ptychoptera sp and Rhyacophila sp.)
-# through summing their abundances
+# through summing their abundances, though influence on results is negligible
+pos_cord <- grep("Cordulegaster", names(taxa_new))
+pos_phil <- grep("Philopotamus", names(taxa_new))
+pos_ptych <- grep("Ptychoptera", names(taxa_new))
+pos_rhya <- grep("Rhyacophila", names(taxa_new))
 
-str(aggr_taxa)
-aggr_taxa[1:67] <- lapply(aggr_taxa[1:67], as.numeric)
-
-aggr_taxa$"Cordulegaster spec." <- rowSums(aggr_taxa[ ,c(10,11)])
-aggr_taxa$"Philopotamus spec." <- rowSums(aggr_taxa[ ,c(38:40)])
-aggr_taxa$"Ptychoptera spec." <- rowSums(aggr_taxa[ ,c(48:49)])
-aggr_taxa$"Rhyacophila spec." <- rowSums(aggr_taxa[ ,c(51:56)])
+taxa_new$"Cordulegaster spec." <- rowSums(taxa_new[ ,pos_cord])
+taxa_new$"Philopotamus spec." <- rowSums(taxa_new[ ,pos_phil])
+taxa_new$"Ptychoptera spec." <- rowSums(taxa_new[ ,pos_ptych])
+taxa_new$"Rhyacophila spec." <- rowSums(taxa_new[ ,pos_rhya])
 
 # remove aggregated taxa and site information
-varetaxa <- aggr_taxa[ ,-c(1,10,11,38:40,48:49,51:56)]
-
+varetaxa <- taxa_new[ ,-c(pos_cord, pos_phil, pos_ptych, pos_rhya)]
+varetaxa <- aggr_taxa
 # We employ the hellinger transformation to use RDA with ecological data 
 # see Legendre, P., Gallagher, E.D., 2001. Ecologically meaningful transformations for ordination of species data. Oecologia 129, 271–280.
 
@@ -330,13 +296,10 @@ varetaxa_hel <- decostand(varetaxa, "hellinger")
 # we obtain 1 RDA (constrained) and 27 PCA (unconstrained) axes
 # 7% explained by the stressor gradient
 
-# Extraction of canonical coefficients from rda object
-coef(VAR_rda)
-
 # Global test of the RDA result
 set.seed(111)
 anova.cca(VAR_rda, step=1000, by='term')
-# grad statistically significant 0.038
+# grad statistically significant 0.039
 
 # order of species along the gradient (RDA 1 axis - constrained variance = 7%)
 sc  <- vegan::scores(VAR_rda, display = 'species', choices = 1, scaling = "species")
@@ -346,7 +309,7 @@ linestack(sc[abs(sc) > 0.07], rownames(sc)[abs(sc) > 0.07], axis = TRUE, cex = 1
 #rownames includes species names
 
 #################################################################
-# 5.   Redundancy Discriminant Analysis for invertebrate traits #
+# 4.   Redundancy Discriminant Analysis for invertebrate traits #
 #################################################################
 
 # RDA with trait modality data
@@ -356,19 +319,63 @@ modalities_hel <- decostand(modalities, "hellinger") # transformation
 #conduct RDA #
 ##############
 
-(mod_rda <- rda(modalities ~ grad))
-# 13% explained by stressor gradient (constrained Proportion 0.127)
-
-coef(mod_rda)
+(mod_rda <- rda(modalities_hel ~ grad))
+# 8% explained by stressor gradient
 
 # Global test of the RDA result
 set.seed(111)
-anova.cca(mod_rda, step=1000) #model statistically significant 
+anova.cca(mod_rda, step=1000, by='term') 
+# model not statistically significant, p = 0.067 - although slightly more variance explained
 
-#order of modalities along stressor gradient (RDA 1 axis - constrained variance = 13%)
+# order of modalities along stressor gradient (RDA 1 axis - constrained variance = 8%)
 sc1  <- scores(mod_rda, display = 'species', choices = 1)
+linestack(sc1[abs(sc1) > .041], trait_labels[abs(sc1) > .041], axis = TRUE, cex = 1.5) 
 
-linestack(sc1[abs(sc1) > .05], rownames(sc1)[abs(sc1) > .05], axis = TRUE, cex = 1.5) 
+############################################################
+## 5.  Diversity metrics and Organic matter breakdown (OMB)
+############################################################
+
+# Analyse relationship between taxonomic and functional diversity and OMB
+
+# Taxonomic diversity with OMB
+m3 <- cor.test(TTR, omb_dat$OMB)
+m4 <- cor.test(SD, omb_dat$OMB)
+p.adjust(c(m3$p.value, m4$p.value), method= "fdr")
+# not significant, but SD p = 0.06
+
+# Gammarid abundance might explain breakdown
+cor.test(tot_gam, omb_dat$OMB)
+# moderate correlation, r = 0.48, p = 0.01
+
+#####################################
+#Functional Diversities and OMB
+#####################################
+
+res_fd <- sapply(FD, function(y) {
+  test <- cor.test(y, omb_dat$OMB, method="pearson")
+})
+
+# extract coef. of correlation
+cor_fd <- res_fd[4, ]
+value_fd <- res_fd[3, ]
+p.adjusted2 <- p.adjust(value_fd, method= "fdr")
+
+# correlation with individual traits?
+m5 <- cor.test(modalities$t10.3, omb_dat$OMB)
+m6 <- cor.test(modalities$t11.3, omb_dat$OMB)
+p.adjust(c(m5$p.value, m6$p.value), method= "fdr")
+# no correlation
+
+# using absolute abundances rather than relative abundances of shredders
+# computation of total trait abundance per site
+trait_abun_matx <- data.frame(as.matrix(abun) %*% as.matrix(prop_trait))
+
+# check summed abundance for specific modalities 
+m7 <- cor.test(trait_abun_matx[ ,"t10.3"], omb_dat$OMB)
+m8 <- cor.test(trait_abun_matx[ ,"t11.3"], omb_dat$OMB)
+
+p.adjust(c(m7$p.value, m8$p.value), method= "fdr")
+# both statistically significant
 
 ######################################
 # Code for figures in article
@@ -385,7 +392,7 @@ linestack(load_spca_n[abs(load_spca_n) > 0], rownames(load_spca_n)[abs(load_spca
 #b)invertebrates taxa
 linestack(sc[abs(sc) > 0.03], rownames(sc)[abs(sc) > 0.03], axis = TRUE, cex =1.1)#rownames includes species names
 #c)trait modalities
-linestack(sc1[abs(sc1) > .035], rownames(sc1)[abs(sc1) > .035], axis = TRUE, cex =1.1)
+linestack(sc1[abs(sc1) > .041], trait_labels[abs(sc1) > .041], axis = TRUE, cex =1.1)
 #dev.off()
 
 #########
